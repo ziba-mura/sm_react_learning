72d5216980092cfaceeebc3fa54db7deac908f01
ziba-mura <ziba-mura@users.noreply.github.com>
2025-03-27 23:25:52 +0900

    TODO追加機能を追加

--- src/components/MainTodo.jsx
+++ src/components/MainTodo.jsx
@@ -2,12 +2,46 @@ import React, { useState } from "react";
 
 export default function MainTodo() {
     const [todo, setTodo] = useState("");
+
+    const [todoList, setTodoList] = useState(() => {
+        const ls = localStorage.getItem("todoList");
+        return ls ? JSON.parse(ls) : [];
+    });
+
+    const addTodo = () => {
+        if (!todo) return;
+        const newTodo = {
+            id: Date.now(),
+            task: todo,
+        };
+        setTodoList((prev) => {
+            const updated = [...prev, newTodo];
+            return updated;
+        });
+        setTodo("");
+    };
+
     return (
         <div>
             <div>
                 <input type="text" className="todo_input" placeholder="+ TODOを入力" value={todo} onChange={(e) => setTodo(e.target.value)} />
-                <button className="btn">追加</button>
-                <p>{todo}</p>
+                <button className="btn" onClick={addTodo}>
+                    追加
+                </button>
+            </div>
+
+            <div className="box_list">
+                {todoList.map((item) => (
+                    <div className="todo_list" key={item.id}>
+                        <div className={`todo`}>
+                            <label>{item.task}</label>
+                        </div>
+                        <div className="btns">
+                            <div className="btn green">編</div>
+                            <div className="btn pink">削</div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
